0d9af8919eac576db891aca0dc88787173c5e1e2
Josiah Davis <josiah.j.davis@gmail.com>
2015-04-06 12:26:11 -0400

    cleaned up the training and testing split

--- CART_RF.R
+++ CART_RF.R
@@ -1,24 +1,23 @@
-#############################
-# Classification Tree
-##############################
+# ============================
+#     Classification Tree
+# ============================
 
 ####
 # Set up the workspace
 ####
-
-rm(list=ls())     # clear the workspace
-set.seed(973487)  # Ensures you can repeat the results
-library(rpart)    # For creating the tree
-library(partykit) # For plotting the tree
-library(vcd)      # For mosaic plot
+rm(list=ls()); gc()     # clear the workspace
+set.seed(973487)        # Ensures you can repeat the results
+library(rpart)          # For creating the tree
+library(partykit)       # For plotting the tree
+setwd("C:/Users/josdavis/Documents/Personal/GitHub/CCI")
 
 #####
 # Get the data
 ####
-load(url('http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic3.sav'))
-rand<-order(runif(dim(titanic3)[1]))  # Random indices
-train <- titanic3[rand[1:655],]       # Training set
-test <- titanic3[rand[656:1309],]     # Testing set
+data <- read.csv("titanic.csv", header = TRUE)
+idxs <- runif(nrow(data)) < 0.7   # Random Indices
+train <- data[idxs, ]             # Training set
+test  <- data[!idxs, ]            # Testing set
 
 #####
 # Create the tree
@@ -27,6 +26,7 @@ form <- as.formula(as.factor(survived) ~ pclass + sex + age + sibsp) # Specify t
 tree <- rpart(form, train)          # Create the tree
 plot(as.party(tree))                # Plot the tree
 round(tree$variable.importance, 1)  # Check the importance (measured as avg. decrease in gini coefficient)
+
 
 #####
 # Evaluate the tree
@@ -44,9 +44,9 @@ labeling_cells(text = labels, margin = 0)(results)
 # Generate plain two way tables
 round(prop.table(table(test$survived,pred.test[,2]>0.5), 1)*100, 2)   # Correctly predicted from test set
 
-#####################
-# Random Forest
-#####################
+# ============================
+#     Random Forest
+# ============================
 library(randomForest)
 rf <- randomForest(form, train, importance = TRUE, na.action = na.omit)
 sort(round(rf$importance[,4], 1), decreasing = TRUE) # Average decrease in the gini coefficient
